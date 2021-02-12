fc7ff2517e1eb6269a3e74c973e6f33ffece323f
Jim Hill <jim.hill.r@gmail.com>
2021-02-12 09:52:04 -0800

    Pushing creds into stdin

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function run() {
       // Execute the docker login command
       let doLoginStdout = '';
       let doLoginStderr = '';
-      const exitCode = await exec.exec('docker login', ['--password-stdin', '-u', creds[0], '-p', creds[1], proxyEndpoint], {
+      const exitCode = await exec.exec(`echo ${creds[1]} | docker login`, ['--password-stdin', '-u', creds[0], proxyEndpoint], {
         silent: true,
         ignoreReturnCode: true,
         listeners: {
